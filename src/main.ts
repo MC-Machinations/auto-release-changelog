8ff7563ee13c4e4a256cc98afa8fecb0bccd701d
Jake Potrebic <jake.m.potrebic@gmail.com>
2021-09-03 20:33:45 -0700

    chore(actions): remove bad start group

--- src/main.ts
+++ src/main.ts
@@ -196,7 +196,6 @@ async function run(): Promise<void> {
         })
         core.endGroup();
 
-        core.startGroup("Gathering commits since last release")
         const commitsSinceRelease = await getCommitsSinceRelease(client, {
             owner: context.repo.owner,
             repo: context.repo.repo,
